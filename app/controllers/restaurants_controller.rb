440d8e3d4bfdb089dd74a858a100620f9edbfef9
tranthuc <thuctc@kaopiz.com>
2016-02-23 17:45:04 +0700

    Marking, search on google map

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,89 @@
+class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+
+  # GET /restaurants
+  # GET /restaurants.json
+  def index
+    # @restaurants = Restaurant.all
+    @q = Restaurant.ransack(params[:q])
+    @restaurants = @q.result(distinct: true)
+    @hash = Gmaps4rails.build_markers(@restaurants) do |restaurant, marker|
+      marker.lat restaurant.latitude
+      marker.lng restaurant.longitude
+      marker.json({:title => restaurant.address, description: restaurant.address })
+      marker.infowindow restaurant.address
+    end
+    # nearbys = Restaurant.near("Ha noi", 500000, :order => "distance")
+    # bearing = nearbys.first.bearing # => 46.12
+    # Geocoder::Calculations.compass_point(bearing) # => "NE"
+    respond_to do |format|
+      format.html
+      format.json { render json: @hash }
+    end
+  end
+
+  # GET /restaurants/1
+  # GET /restaurants/1.json
+  def show
+  end
+
+  # GET /restaurants/new
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  # GET /restaurants/1/edit
+  def edit
+  end
+
+  # POST /restaurants
+  # POST /restaurants.json
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+
+    respond_to do |format|
+      if @restaurant.save
+        format.html { redirect_to @restaurant, notice: 'Restaurant was successfully created.' }
+        format.json { render :show, status: :created, location: @restaurant }
+      else
+        format.html { render :new }
+        format.json { render json: @restaurant.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /restaurants/1
+  # PATCH/PUT /restaurants/1.json
+  def update
+    respond_to do |format|
+      if @restaurant.update(restaurant_params)
+        format.html { redirect_to @restaurant, notice: 'Restaurant was successfully updated.' }
+        format.json { render :show, status: :ok, location: @restaurant }
+      else
+        format.html { render :edit }
+        format.json { render json: @restaurant.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /restaurants/1
+  # DELETE /restaurants/1.json
+  def destroy
+    @restaurant.destroy
+    respond_to do |format|
+      format.html { redirect_to restaurants_url, notice: 'Restaurant was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_restaurant
+      @restaurant = Restaurant.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def restaurant_params
+      params.require(:restaurant).permit(:name, :address)
+    end
+end
